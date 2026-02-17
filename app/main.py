67daaa2810f96fc057e04fb0b9ae0121bd3adc2e
Gül Sena Altıntaş <30217912+gsaltintas@users.noreply.github.com>
2026-02-17 17:51:05 -0500

    Updated the middleware to allow for the render backend

--- app/main.py
+++ app/main.py
@@ -27,9 +27,19 @@ app = FastAPI(
     lifespan=lifespan,
 )
 
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+
+# MUST be BEFORE including routers
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["*"],  # Allow all origins for now
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
